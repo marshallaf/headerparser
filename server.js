b9dc1ac92533ece374d6144461e34a6de8c33aeb
Andrew Marshall <andrew.f.marshall@gmail.com>
2017-03-17 03:30:03 +0000

    added header parsing

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ var app = express();
 // set port
 app.set('port', (process.env.PORT || 8080));
 
-// otherwise, collect date and try to parse it
 app.get('/', (req, res) => {
     const whoami = {};
     if (req.ip.startsWith('::ffff:')) {
         whoami.ipaddress = req.ip.slice(7);
     } else whoami.ipaddress = req.ip;
+    whoami.language = req.get('Accept-Language').split(',')[0];
+    whoami.software = req.get('User-Agent').split(/[\(\)]/)[1];
     res.send(whoami);
 });
 
